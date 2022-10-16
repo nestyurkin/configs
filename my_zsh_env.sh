d210a9541bc170f6ffb26f3fc6f6247ea60423f4
Николай Нестюркин <n.nestyurkin@superjob.ru>
2022-10-16 23:06:26 +0300

    fix zsh version chk

--- my_zsh_env.sh
+++ my_zsh_env.sh
@@ -34,15 +34,17 @@ rm -rf ~/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#fix grou perms
+#fix group perms
 chmod g-w -R ~/.oh-my-zsh
 #install configs
-zshv=$(zsh -c "echo $ZSH_VERSION")
-if [ $(echo "${zshv}\n5.1"|sort|head -1) != "${zshv}" ]; then 
+zshv=(`echo $(zsh --version) | tr ' ' ' '`)
+if [ $(echo -e "${zshv[1]}\n5.1"|sort -V|head -1) != "${zshv[1]}" ]; then 
+    echo "ZSH >=5.1"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     curl -L -o ~/.p10k.zsh https://raw.githubusercontent.com/nestyurkin/configs/main/.p10k.comp.zsh
     curl -L -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc
 else 
+    echo "ZSH < 5.1 to old"
     curl -L -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc_old
 fi
 
