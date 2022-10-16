b1d7737c3fb8a45e12064720eb7d051ed37cfdae
Николай Нестюркин <n.nestyurkin@superjob.ru>
2022-10-17 01:25:05 +0300

    silent

--- my_zsh_env.sh
+++ my_zsh_env.sh
@@ -25,7 +25,7 @@ if [ -n "$pygm" ]; then
 else
     echo Install Chroma
     tmp=$(mktemp -d)
-    curl -L -o $tmp/chroma.tar.gz https://github.com/alecthomas/chroma/releases/download/v2.3.0/chroma-2.3.0-linux-amd64.tar.gz
+    curl -fsSL -o $tmp/chroma.tar.gz https://github.com/alecthomas/chroma/releases/download/v2.3.0/chroma-2.3.0-linux-amd64.tar.gz
     tar -xf $tmp/chroma.tar.gz -C $tmp
     sudo cp $tmp/chroma /usr/local/bin/
 fi
@@ -35,7 +35,7 @@ isldap=$(cat /etc/passwd | grep $USER)
 if [ -n "$isldap" ]; then
     sudo usermod --shell /bin/zsh "$USER"
 else
-    curl -L -o ~/.bashrc https://raw.githubusercontent.com/nestyurkin/configs/main/.bashrc
+    curl --fsSL -o ~/.bashrc https://raw.githubusercontent.com/nestyurkin/configs/main/.bashrc
 fi
 
 #cleanup
@@ -43,18 +43,18 @@ rm ~/.p10k.zsh ~/.zshrc
 rm -rf ~/.oh-my-zsh/
 #install plugins & theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 #install configs
 zshv=$(zsh --version | cut -d' ' -f 2)
 if [ $(echo -e "${zshv}\n5.1"|sort -V|head -1) != "${zshv}" ]; then 
     echo "ZSH >=5.1"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    curl -L -o ~/.p10k.zsh https://raw.githubusercontent.com/nestyurkin/configs/main/.p10k.comp.zsh
-    curl -L -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc
+    git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    curl -fsSL -o ~/.p10k.zsh https://raw.githubusercontent.com/nestyurkin/configs/main/.p10k.comp.zsh
+    curl -fsSL -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc
 else 
     echo "ZSH < 5.1 to old"
-    curl -L -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc_old
+    curl -fsSL -o ~/.zshrc https://raw.githubusercontent.com/nestyurkin/configs/main/.zshrc_old
 fi
 #fix group perms
 chmod g-w -R ~/.oh-my-zsh
